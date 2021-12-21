f811d5aef3eea462a2f062d89f2e4143efed65da
TurboGoose <antonpuckich@gmail.com>
2021-12-21 22:30:28 +0300

    Add CORS access-control-allow-origin support

--- backend/src/main/java/com/turbogoose/Flashcard/io/controller/CardsController.java
+++ backend/src/main/java/com/turbogoose/Flashcard/io/controller/CardsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("decks/{deckId}/cards")
 public class CardsController {
